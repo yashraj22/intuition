e5bb5ef4979233bb0f315d249977c1575dce061b
Gaurav kumar <indiagauravkumar@gmail.com>
2025-03-09 14:45:10 +0530

    Temp :temporary change in the type definition

--- types/feed.ts
+++ types/feed.ts
@@ -15,10 +15,22 @@ export interface Source {
   name: string;
 }
 
+export interface SavedItem {
+
+  id: string;
+  title: string;
+  description: string; // Make sure description is always a string
+  imageUrl: string | null; // Make sure imageUrl is always a string
+  url: string; // Add url property
+  saved: boolean;
+  author: string;
+
+}
+
 
 
 //type for the save item, it will be exactly the same as the FeedItem, this is 
 /// for distinguish the FeedItem from the saved item
 
 // Interface for saved items, extending FeedItem to distinguish from regular feed items
-export type SavedItem = FeedItem
+// export type SavedItem = FeedItem
