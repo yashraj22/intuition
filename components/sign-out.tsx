13f3b9b20c3962ca10c9cfa9424c12334b8093fd
Gaurav kumar <indiagauravkumar@gmail.com>
2025-03-09 08:23:55 +0530

    Feature: Add avatar and dropdown menu components for user sign-out functionality

--- components/sign-out.tsx
+++ components/sign-out.tsx
@@ -1,19 +1,38 @@
 "use client";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function SignOut() {
+  const { data: session } = useSession();
   const handleButtonClick = async () => {
     // Initiate Google sign-in
     await signOut();
   };
 
   return (
-    <button
-      onClick={handleButtonClick}
-      className="text-white border border-white px-3 py-1 rounded-lg"
-    >
-      Sign Out
-    </button>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={session?.user?.image || ""} />
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className = "cursor-pointer" onClick={handleButtonClick}>
+          Sign Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
