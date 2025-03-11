a147270310db877da9566db48cda337c4450d4ab
yashraj22 <yyashraj05@gmail.com>
2025-03-11 22:27:25 +0530

    chore : Fix type issue and useEffect infinite loop

--- app/api/saved/route.ts
+++ app/api/saved/route.ts
@@ -1,81 +1,77 @@
-
-import { NextResponse, NextRequest} from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { auth } from "@/auth"; // Adjust the path to your auth.ts
-import prisma from '@/lib/prisma';
-
-
-
+import prisma from "@/lib/prisma";
 
 export async function GET() {
-  const session = await auth();
-  const userId = session?.user?.id;
+	const session = await auth();
+	const userId = session?.user?.id;
 
-  try {
-      const savedItems = await prisma.saved.findMany({
-          where: {
-              userId: userId,
-          },
-          include: {
-              feedItem: true, // Include the related FeedItem data
-          },
-          orderBy: { createdAt: "desc" },
-      });
+	try {
+		const savedItems = await prisma.saved.findMany({
+			where: {
+				userId: userId,
+			},
+			include: {
+				feedItem: true, // Include the related FeedItem data
+			},
+			orderBy: { createdAt: "desc" },
+		});
 
-      return NextResponse.json({
-          success: true,
-          data: savedItems,
-      });
-  } catch (error) {
-      console.error("Error fetching saved items:", error);
-      return NextResponse.json(
-          { success: false, message: "Failed to fetch saved items" },
-          { status: 500 }
-      );
-  }
+		return NextResponse.json({
+			success: true,
+			data: savedItems,
+		});
+	} catch (error) {
+		console.error("Error fetching saved items:", error);
+		return NextResponse.json(
+			{ success: false, message: "Failed to fetch saved items" },
+			{ status: 500 },
+		);
+	}
 }
-
-
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const { userId, feedItemID } = await body;
-  try {
-    await prisma.saved.create({
-      data: {
-        userId: userId as string,
-        feedItemId: feedItemID as string,
-      },
-    });
-    return new NextResponse("Post saved successfully!", { status: 201 });
-  } catch (error) {
-    console.error("Error creating message:", error);
-  }
+	const body = await req.json();
+	const { userId, feedItemID } = await body;
+	try {
+		await prisma.saved.create({
+			data: {
+				userId: userId as string,
+				feedItemId: feedItemID as string,
+			},
+		});
+		return NextResponse.json(
+			{ success: true, message: "Post saved successfully!", data: response },
+			{ status: 201 },
+		);
+	} catch (error) {
+		console.error("Error creating message:", error);
+	}
 }
 
-
-
 export async function DELETE(req: NextRequest) {
-  const session = await auth();
-  const userId = session?.user?.id;
-  try {
-    const body = await req.json();
-    const { itemId } = body;
+	const session = await auth();
+	const userId = session?.user?.id;
+	try {
+		const body = await req.json();
+		const { itemId } = body;
 
-    await prisma.saved.delete({
-      where: {
-        id: itemId, // Use itemId to delete the specific saved item
-        userId: userId,
-      },
-    });
+		await prisma.saved.delete({
+			where: {
+				id: itemId, // Use itemId to delete the specific saved item
+				userId: userId,
+			},
+		});
 
-    return NextResponse.json({ success: true, message:"Post Deleted successfully!"}, { status: 200 }
-      
-    );
-  } catch (error) {
-    console.error("Error Deleting post", error);
-    return NextResponse.json(
-      { success: false, message: "Failed to delete saved item" },
-      { status: 500 }
-    );
-  }
+		return NextResponse.json(
+			{ success: true, message: "Post Deleted successfully!" },
+			{ status: 200 },
+		);
+	} catch (error) {
+		console.error("Error Deleting post", error);
+		return NextResponse.json(
+			{ success: false, message: "Failed to delete saved item" },
+			{ status: 500 },
+		);
+	}
 }
