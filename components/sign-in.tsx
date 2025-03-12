a9f27a2f621278109d8af08933b02c74ca54a414
Gaurav kumar <indiagauravkumar@gmail.com>
2025-03-12 18:59:16 +0530

    Feature: Refactor sign-in button to use Avatar component with user icon

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { Avatar } from "@/components/ui/avatar";
+import { User } from "lucide-react";
 
 export default function SignIn() {
   const handleButtonClick = async () => {
     // Initiate Google sign-in
     await signIn("google");
   };
-
   return (
-    <button
-      onClick={handleButtonClick}
-      className="text-white border border-white px-3 py-1 rounded-lg"
-    >
-      Sign in
-    </button>
+    <Avatar>
+      <button className="text-white" onClick={handleButtonClick}>
+        <User />
+      </button>
+    </Avatar>
   );
 }
